7097f1c1900b12b7af20041e7b09ff873ee248a8
Anton Boyko <me@boykoant.pro>
2020-05-01 22:06:26 +0300

    started with tests automation

---
+++ ServerlessCommunity.Test.Integration/Data/Service/VenueServiceTest.cs
@@ -0,0 +1,15 @@
+using Microsoft.WindowsAzure.Storage.Table;
+using NUnit.Framework;
+using ServerlessCommunity.Application.Data.Service;
+
+namespace ServerlessCommunity.Test.Integration.Data.Service
+{
+    [TestFixture]
+    public class VenueServiceTest
+    {
+        private CloudTable _table;
+        private IVenueService _service;
+
+        
+    }
+}
